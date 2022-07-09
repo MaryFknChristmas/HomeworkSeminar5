1f0bd5460a91b661da6ee47be6b9066340390d31
Marina <gtellier.mecanics@gmail.com>
2022-07-09 16:31:40 +0300

    Сумма чисел с нечетными позициями

---
+++ task2/Program.cs
@@ -0,0 +1,42 @@
+﻿int size = 10;
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Сумма элементов, стоящих на нечетных позициях, равна: " + SumNumbers(array));
+
+int SumNumbers(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 1)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+
+
+
+
+
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-10, 20);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
