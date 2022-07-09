32f7f11c7fd9d48f2fdfca5077d60f9f96a17936
Marina <gtellier.mecanics@gmail.com>
2022-07-09 16:23:54 +0300

    Кол-во четных чисел в массиве

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿int size = 10;
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Количество четных чисел в массиве равно: " + CountPositive(array));
+
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
